09b1a9c7e2b7c63f5f6936b4b2684243658cb35a
Chella <mchellamuthu@live.com>
2022-09-14 10:00:23 +0530

    initial commit

---
+++ app/Console/Commands/LoadData.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class LoadData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'load:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Data Loading';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
---
+++ database/migrations/2022_09_14_042036_create_histories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->string('symbols');
+            $table->decimal('open');
+            $table->decimal('close')->nullable();
+            $table->decimal('low')->nullable();
+            $table->decimal('volume')->nullable();
+            $table->decimal('last')->nullable();
+            $table->dateTime('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+};
